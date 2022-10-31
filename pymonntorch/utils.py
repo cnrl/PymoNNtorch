1224696625e27fb3208c2d1d3fe86dd42e608586
atenagm1375 <ashenatena@gmail.com>
2022-10-31 10:07:30 +0330

    add utility function to make the given variable is torch compatible if it is not so

--- pymonntorch/utils.py
+++ pymonntorch/utils.py
@@ -1,3 +1,6 @@
+import torch
+
+
 def is_number(s):
     """ Returns True if string is a number. """
     try:
@@ -5,3 +8,8 @@ def is_number(s):
         return True
     except ValueError:
         return False
+
+
+def check_is_torch_tensor(x, device='cpu', dtype=torch.float32):
+    if not torch.is_tensor(x):
+        return torch.tensor(x, device=device, dtype=dtype)
