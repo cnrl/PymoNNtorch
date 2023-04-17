c38d4ff49941ac2656ab1502c9495c9af4499b7c
PesarAmmehZA <saeed.ali.rahimi@gmail.com>
2023-04-17 12:00:48 +0330

    Doc (#8)
    * setup
    
    * Update .travis.yml
    
    * Update tox.ini
    
    * readthedoc
    
    * os
    
    * requirements
    
    ---------
    
    Co-authored-by: Amir.H Ebrahimi <amirhosseinebrahimi77@gmail.com>

--- setup.py
+++ setup.py
@@ -17,15 +17,15 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Ashena Gorgan Mohammadi",
     author_email='ashenatena@gmail.com',
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+#        'Programming Language :: Python :: 3.6',
+#        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
     description="PymoNNtorch is a Pytorch version of PymoNNto",
